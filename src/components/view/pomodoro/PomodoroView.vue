77fd86f5ea4ab368e2d84ba610888c54d9e4e813
Guilherme Lima <guilhermevictorrocha@gmail.com>
2022-06-16 22:35:41 -0300

    feat: criando view do timer pomodoro

---
+++ src/components/view/pomodoro/PomodoroView.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="text-center">
+		<span>
+			<h1>Pomodoro</h1>
+			<h1>Tracker</h1>
+		</span>
+		<div>
+			<TemporizadorComponent :tempoDoCiclo="120" :isTemporizadorRodando="isTemporizadorRodando"/>
+			<button @click="isTemporizadorRodando = true">Oi</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TemporizadorComponent from '@/components/shared/temporizador/TemporizadorComponent.vue';
+
+export default defineComponent({
+	name: 'PomodoroView',
+	
+	components: {
+		TemporizadorComponent
+	},
+
+	data() {
+		return {
+			isTemporizadorRodando: false
+		}
+	},
+
+});
+</script>
+
+<style>
+
+</style>
